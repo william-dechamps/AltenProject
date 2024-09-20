8ef4ed28eeecd8936a29ac204868c7838e60b8ef
William <william-dechamps@hotmail.com>
2024-09-20 14:34:40 +0200

    Use Alten project template for front

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  Component,
+} from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { SplitterModule } from 'primeng/splitter';
+import { ToolbarModule } from 'primeng/toolbar';
+import { PanelMenuComponent } from "./shared/ui/panel-menu/panel-menu.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterModule, SplitterModule, ToolbarModule, PanelMenuComponent],
 })
 export class AppComponent {
-  title = 'AltenProject';
+  title = "ALTEN SHOP";
 }
--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,36 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { enableProdMode, importProvidersFrom } from "@angular/core";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { registerLocaleData } from "@angular/common";
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from "@angular/common/http";
+import localeFr from "@angular/common/locales/fr";
+import { BrowserModule, bootstrapApplication } from "@angular/platform-browser";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideRouter } from "@angular/router";
+import { APP_ROUTES } from "app/app.routes";
+import { ConfirmationService, MessageService } from "primeng/api";
+import { DialogService } from "primeng/dynamicdialog";
+import { AppComponent } from "./app/app.component";
+import { environment } from "./environments/environment";
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule),
+    provideHttpClient(
+      withInterceptorsFromDi(),
+    ),
+    provideAnimations(),
+    provideRouter(APP_ROUTES),
+    ConfirmationService,
+    MessageService,
+    DialogService,
+  ],
+}).catch((err) => console.log(err));
+
+registerLocaleData(localeFr, "fr-FR");
