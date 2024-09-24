49d21fd067ac0543ddeab0d69f4fcbd5eae1be37
William <william-dechamps@hotmail.com>
2024-09-24 19:43:11 +0200

    add contact form with validation

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./shared/features/home/home.component";
 import { CartComponent } from "./cart/cart.component";
+import { ContactFormComponent } from "./contact/contact-form.component";
 
 export const APP_ROUTES: Routes = [
   {
@@ -15,6 +16,10 @@ export const APP_ROUTES: Routes = [
   {
     path: "cart",
     component: CartComponent,
+  },
+  {
+    path: "contact",
+    component: ContactFormComponent,
   },
   { path: "", redirectTo: "home", pathMatch: "full" },
 ];
